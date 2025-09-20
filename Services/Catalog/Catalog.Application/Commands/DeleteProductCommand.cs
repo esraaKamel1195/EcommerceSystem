00a35fb23bdb00179df4caf23755d2536a449ca5
Esraa Kamel <esraa.kamel1811@gmail.com>
2025-09-20 13:43:55 +0300

    Implement product commands

---
+++ Services/Catalog/Catalog.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Commands
+{
+    public class DeleteProductCommand: IRequest<bool>
+    {
+        public string Id { get; set; }
+
+        public DeleteProductCommand(string id)
+        {
+            Id = id;
+        }
+    }
+}
