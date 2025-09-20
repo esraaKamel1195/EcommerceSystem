99635446295d2804f9e2fcb7e18a226c2dab1f28
Esraa Kamel <esraa.kamel1811@gmail.com>
2025-09-20 17:39:24 +0300

    Fix database collection string and soluation files

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,11 @@
 
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data.Context;
+using Catalog.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Catalog.API;
 
@@ -14,6 +20,23 @@ public class Program
 
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+        builder.Services.AddAutoMapper(typeof(ProductMappingProfile).Assembly);
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+            Assembly.GetExecutingAssembly()
+           ,Assembly.GetAssembly(typeof(GetProductByIdQuery))));
+
+        builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<IBrandRepository, ProductRepository>();
+        builder.Services.AddScoped<ITypeRepository, ProductRepository>();
+
+        builder.Services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+        });
 
         // Replace the incorrect AddSwaggerGen usage with the correct overload that takes an options lambda.
         builder.Services.AddSwaggerGen(options =>
@@ -34,7 +57,7 @@ public class Program
 
         builder.Services.AddOpenApi();
 
-        var app = builder.Build();
+        var app = builder.Build(); 
 
         //app.MapDefaultEndpoints();
 
