4e74db64b45b264053fd03a3b70c1a8dc72661fa
Esraa Kamel <esraa.kamel1811@gmail.com>
2025-09-20 13:44:36 +0300

    Handle product commands

---
+++ Services/Catalog/Catalog.Application/Handlers/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductResponseDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductCommandHandler(IMapper mapper, IProductRepository productRepository) {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponseDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = _mapper.Map<Product>(request);
+            //if (productEntity == null) {
+                var newProduct = await _productRepository.CreateProduct(productEntity);
+                var productResponse = _mapper.Map<ProductResponseDto>(newProduct);
+                return productResponse;
+            //}
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Handlers/Commands/UpdateProductCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Catalog.Application.Commands;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Handlers.Commands
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, bool>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+
+        public UpdateProductCommandHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productEntity = _mapper.Map<Product>(request);
+            var updateProduct = await _productRepository.UpdateProduct(productEntity);
+            return updateProduct;
+        }
+
+        //try other handle way
+        /*public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var updateProduct = await _productRepository.UpdateProduct(new Product()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Brands = request.Brands,
+                Types = request.Types,
+                Summary = request.Summary,
+                Price = request.Price,
+            });
+
+            return updateProduct;
+        }*/
+    }
+}
